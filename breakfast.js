c036b40c87ca40e4dfdef124814516f33ee504ea
Austin C <36517066+AustinCraver@users.noreply.github.com>
2019-03-14 16:12:11 -0500

    Create breakfast.js

---
+++ breakfast.js
@@ -0,0 +1,62 @@
+/* Breakfast with JavaScript written by Austin Craver 
+   for a project via Khan Academy. 
+*/
+
+// wooden tableb - code was pre-written
+background(186, 145, 20);
+
+//plate - code was pre-written
+ellipse(200, 200, 350, 350); 
+ellipse(200, 200, 300, 300); 
+
+// start original code:
+
+//bacon1
+fill(252, 60, 115);
+rect(76, 149, 33, 112);
+
+//bacon2
+fill(252, 60, 115);
+rect(114, 157, 33, 112);
+
+//bacon3
+fill(252, 60, 115);
+rect(154, 141, 33, 112);
+
+//sausage1
+fill(176, 113, 12);
+ellipse(200, 300, 71, 71);
+
+//sausage2
+fill(176, 113, 12);
+ellipse(221, 306, 68, 68);
+
+//bottom egg white
+fill(240, 238, 235);
+ellipse(267, 159, 132, 98);
+
+//bottom egg yolk
+fill(251, 255, 8);
+ellipse(267, 158, 52, 43);
+
+//top egg white
+fill(240, 238, 235);
+ellipse(266, 208, 118, 82);
+
+//top egg yolk
+fill(251, 255, 8);
+ellipse(258, 212, 43, 43);
+
+//toast
+fill(212, 147, 93);
+triangle(119, 83, 208, 81, 104, 142);
+
+
+
+
+
+
+
+
+
+
